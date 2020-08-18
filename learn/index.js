41ec39af2b3025f364be7bd2e560e87109662c1f
Rohan Kapur <adada.rohan@gmail.com>
2020-08-19 00:15:46 +0530

    added search functionality

--- learn/index.js
+++ learn/index.js
@@ -23,11 +23,31 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 function flip ( card_id , state) {
     if (state) {
-        document.getElementById(card_id).classList.add("d-none");
+        document.getElementById(card_id + "-front").classList.add("d-none");
         document.getElementById(card_id + "-back").classList.remove("d-none");
     }
     else {
-        document.getElementById(card_id).classList.remove("d-none");
+        document.getElementById(card_id + "-front").classList.remove("d-none");
         document.getElementById(card_id + "-back").classList.add("d-none");
     }
 }
+
+var languages = ['example' , 'example2']
+
+function search (query) {
+    const matches = languages.filter(s => s.includes(query));
+    languages.forEach(element => {
+        if (matches.includes(element) == false) {
+            document.getElementById(element).classList.add("d-none");
+        } else {
+            document.getElementById(element).classList.remove("d-none");
+        }
+    });
+}
+
+document.getElementById('languages-search').onkeydown = function(e){
+    if(e.keyCode == 13){
+      query = document.getElementById("languages-search").value ;
+      search(query)
+    }
+ };
