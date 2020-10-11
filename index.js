d6c2626adf8275cad9b3af4f5c9286dcb4755a0f
Rohan Kapur <adada.rohan@gmail.com>
2020-10-11 13:46:05 +0530

    removed user roles

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        document.getElementById("get-started").href = "/bhashaflix/learn";
-        document.getElementById("learn-card").href = "/bhashaflix/learn";
-        document.getElementById("get-started-bottom").href = "/bhashaflix/learn";
+        var nextlink = "/bhashaflix/learn" ;
+        document.getElementById("get-started").href = nextlink;
+        document.getElementById("learn-card").href = nextlink;
+        document.getElementById("get-started-bottom").href = nextlink;
     }
 })
 
