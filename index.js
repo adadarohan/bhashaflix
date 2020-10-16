1610a5d7539e30cf6b09e54321b1b68c10e2c490
Rohan Kapur <adada.rohan@gmail.com>
2020-10-16 14:37:09 +0530

    changed home pic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        var nextlink = "/bhashaflix/learn" ;
+        var nextlink = "/learn" ;
         document.getElementById("get-started").href = nextlink;
         document.getElementById("learn-card").href = nextlink;
         document.getElementById("get-started-bottom").href = nextlink;
