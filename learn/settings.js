b54dd494d487a09feb42532e7c18f9e92346997c
Rohan Kapur <adada.rohan@gmail.com>
2020-11-21 00:10:12 +0530

    fixed signout redirect bug

--- learn/settings.js
+++ learn/settings.js
@@ -1,10 +1,9 @@
 function sign_out () {
     firebase.auth().signOut().then(function() {
         console.log('Signed Out');
-        window.location.href = "/bhashaflix";
+        window.location.href = "/";
       }, function(error) {
           console.error('Sign Out Error', error);
       });
-    
 
 }
